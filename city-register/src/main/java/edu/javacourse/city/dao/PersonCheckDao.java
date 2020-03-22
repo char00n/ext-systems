5d895f74f49f5256e5a1494ef579974ef7a34110
Charon <ap.lisnevsky@gmail.com>
2020-03-23 00:04:58 +0200

    Issue 87

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -4,19 +4,53 @@ import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
 import edu.javacourse.city.exception.PersonCheckException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PersonCheckDao {
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST =
+            "select temporal from cr_address_person ap " +
+            "inner join cr_person p ON p.person_id = ap.person_id " +
+            "inner join cr_address a ON a.address_id = ap.address_id " +
+            "where " +
+            "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.end_date or ap.end_date is null)" +
+            "and upper(p.sur_name) = upper(?) " +
+            "and upper(p.given_name) = upper(?) " +
+            "and upper(p.patronymic) = upper(?) " +
+            "and p.date_of_birth = ? " +
+            "and a.street_code = ? " +
+            "and upper(a.building) = upper(?) ";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
 
+        String sql = SQL_REQUEST;
+        if (request.getExtension() != null) {
+            sql += "and upper(a.extension) = upper(?) ";
+        } else {
+            sql += "and extension is null ";
+        }
+        if (request.getApartment() != null) {
+            sql += "and upper(a.apartment) = upper(?) ";
+        } else {
+            sql += "and a.apartment is null ";
+        }
+
         try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(SQL_REQUEST) ) {
+             PreparedStatement stmt = con.prepareStatement(sql) ) {
+
+            int count = 1;
+            stmt.setString(count++, request.getSurName());
+            stmt.setString(count++, request.getGivenName());
+            stmt.setString(count++, request.getPatronymic());
+            stmt.setDate(count++, java.sql.Date.valueOf(request.getDateOfBirth()));
+            stmt.setInt(count++, request.getStreetCode());
+            stmt.setString(count++, request.getBuilding());
+            if (request.getExtension() != null) {
+                stmt.setString(count++, request.getExtension());
+            }
+            if (request.getApartment() != null) {
+                stmt.setString(count++, request.getApartment());
+            }
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
@@ -30,7 +64,8 @@ public class PersonCheckDao {
         return response;
     }
 
-    private Connection getConnection() {
-        return null;
+    private Connection getConnection() throws SQLException{
+        return DriverManager.getConnection("jdbc:postgresql://localhost/city_register",
+                "postgres", "Postgres");
     }
 }
--- city-register/src/main/resources/register.sql
+++ city-register/src/main/resources/register.sql
@@ -32,14 +32,17 @@ CREATE TABLE cr_address (
 	street_code integer not null,
 	building varchar(10) not null,
 	extension varchar(10),
-	appartment varchar(10),
+	apartment varchar(10),
 	PRIMARY KEY (address_id),
 	FOREIGN KEY (street_code) REFERENCES cr_street(street_code) ON DELETE RESTRICT,
 	FOREIGN KEY (district_code) REFERENCES cr_district(district_code) ON DELETE RESTRICT
 );
 
-INSERT INTO cr_address (district_code, street_code, building, extension, appartment )
+INSERT INTO cr_address (district_code, street_code, building, extension, apartment )
 VALUES (1, 1, '10', '2', '121');
+
+INSERT INTO cr_address (district_code, street_code, building, extension, apartment )
+VALUES (1, 1, '271', null, '4');
 
 CREATE TABLE cr_person (
     person_id SERIAL,
@@ -87,7 +90,7 @@ INSERT INTO cr_address_person (address_id, person_id, start_date, end_date, temp
 VALUES (1, 1, '2014-10-12', null, false);
 
 INSERT INTO cr_address_person (address_id, person_id, start_date, end_date )
-VALUES (1, 2, '2014-10-12', null);
+VALUES (2, 2, '2014-10-12', null);
 
 INSERT INTO cr_address_person (address_id, person_id, start_date, end_date )
 VALUES (1, 3, '2016-02-05', null);
